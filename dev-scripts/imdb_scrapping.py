af5f44bb9696587ab60c96c4aa69d151236dca47
Christophe Kevers <christophe.kevers@luminus.be>
2021-07-30 18:03:13 +0200

    getting text on web is partially done

--- dev-scripts/imdb_scrapping.py
+++ dev-scripts/imdb_scrapping.py
@@ -72,20 +72,57 @@ for i in range(1, 5):
 """Testing on ultimate tennis website"""
 
 options = Options()
-# options.add_argument("--headless")
-options.add_argument(r"user-data-dir=C:\Users\kevec\AppData\Local\Google\Chrome\User Data\Default")
+#options.add_argument("--headless")
+#options.add_argument(r"user-data-dir=C:\Users\kevec\AppData\Local\Google\Chrome\User Data\Default")
 
 browser = webdriver.Chrome(r'C:\Users\kevec\chromedriver.exe')
 
 browser.get('https://www.ultimatetennisstatistics.com/rankingsTable')
 
 
-for i in range(1, 5):
-    #Get link to each player and write name of the player in csv file
-    player = WebDriverWait(browser, 20).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="rankingsTable"]/tbody/tr['+str(i)+']/td[4]/a')))
-    print(player)
-    tennis_csv.write(player.text + ',')
+#for i in range(1, 5):
+#Get link to each player and write name of the player in csv file
+player = WebDriverWait(browser, 20).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="rankingsTable"]/tbody/tr[1]/td[4]/a')))
+print(player)
+tennis_csv.write(player.text + ',')
 
-    # click on each generated link to go to player's bio, get "turnet pro" info and write it to the file
-    WebDriverWait(browser, 100).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="rankingsTable"]/tbody/tr['+str(i)+']/td[4]/a'))).click()
+# click on each generated link to go to player's bio, get "turnet pro" info and write it to the file
+WebDriverWait(browser, 100).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="rankingsTable"]/tbody/tr[1]/td[4]/a'))).click()
+turned_pro = WebDriverWait(browser, 100).until(expected_conditions.visibility_of_element_located((By.XPATH, '//*[@id="playerProfileHero"]/div[2]/div[2]/div/table/tbody/tr[1]/td[2]/div/div[1]'))).text
+print(turned_pro)
+career_length = 2019 - int(turned_pro)
+tennis_csv.write(str(career_length) + ',')
+
+"""Other method to click (close to JS)"""
+
+
+# element = browser.find_elements_by_tag_name("a")
+# print(type(element))
+# for i in element:
+#     if i.text == "Novak Djokovic":
+#         query = i
+#         print(i)
+#         print(i.text)
+
+empty_list = []
+for i in range(1, 1):
+    player = browser.find_elements_by_xpath(f'//*[@id="rankingsTable"]/tbody/tr[{i}]/td[4]/a')
+    print(type(player))
+    browser.execute_script("arguments[0].click();", player)
+    print(type(player))
+    for i in player:
+        print(i.text)
+        empty_list.append(i.text)
+
+print(empty_list)
+
+    # for top in djoko:
+    #     click_on_it = top
+    #     print(top)
+    #     print(top.text)
+
+# player = browser.find_elements_by_xpath('//*[@id="profile"]/div/div[1]/table/tbody/tr[4]/td')
+# print(type(players))
+# for i in players:
+#     print(i.text)
 
